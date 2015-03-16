dffce98250a3586d72d9061ee5c8f84c92a2273b
Elliott Young <elliott.a.young@gmail.com>
2015-03-16 12:36:43 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/corporate' => 'faker#corporate', as: 'corporate_request'
+  get '/hacker'    => 'faker#hacker',    as: 'hacker_request'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

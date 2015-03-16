d9c6fcfc56e7a3157b1695c8b4ec43eeadcef22b
Elliott Young <elliott.a.young@gmail.com>
2015-03-16 14:38:38 -0500

    Adding Bootstrap

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -13,4 +13,32 @@ $(document).ready(function(){
       $("#saying").empty().append(response.saying);
     });
   });
+
+  $("#corporate-saying").on("click", function(e){
+    e.preventDefault();
+    serverUrl = '/corporate'
+
+    var request = $.ajax({
+      url: serverUrl,
+      type: 'get'
+    });
+
+    request.done(function(response){
+      $("#saying").empty().append(response.saying);
+    });
+  });
+
+  $("#hacker-saying").on("click", function(e){
+    e.preventDefault();
+    serverUrl = '/hacker'
+
+    var request = $.ajax({
+      url: serverUrl,
+      type: 'get'
+    });
+
+    request.done(function(response){
+      $("#saying").empty().append(response.saying);
+    });
+  });
 });
